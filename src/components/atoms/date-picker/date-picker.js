2af5e6c5dcf4a131daea5bfe022e893355b94fba
Tobias Andersson <tobiasgustaverik@icloud.com>
2021-04-22 15:10:59 +0200

    fixed the review comments

--- src/components/atoms/date-picker/date-picker.js
+++ src/components/atoms/date-picker/date-picker.js
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
-import DatePicker from "react-datepicker";
+import datepicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-function datePicker() {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const [selectedDate, setSelectedDate] = useState(null);
+const DatePicker = ({ selectedDate, setSelectedDate }) => {
+  
   return (
-    <div className="container">
-      <DatePicker
+    <div className="date-picker">
+      <datepicker
         placeholderText="Pick your date"
         selected={selectedDate}
         onChange={(date) => setSelectedDate(date)}
@@ -16,4 +15,4 @@ function datePicker() {
   );
 }
 
-export default datePicker;
+export default DatePicker;
