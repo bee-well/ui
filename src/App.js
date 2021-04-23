400099e7b29ecb3185393a9e579a7988ae25b091
Rasmus Nilsson <info@rasmusnilsson.se>
2021-04-23 14:28:13 +0200

    fixed onSignUp issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import {useState} from "react"
+import SignUpModal from "./components/organisms/modals/sign-up-modal"
+
 function App() {
+  const [open, setOpen] = useState(false)
+
   return (
     <div className="App">
+      <button onClick={() => setOpen(true)}>test</button>
+      <SignUpModal 
+        open={open}
+        onClose={() => setOpen(false)}
+        onSignUp={() => alert("sign up")}
+      />
     </div>
   );
 }
