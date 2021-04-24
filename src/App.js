3a0deb0ba3c90a30c0096cdb4594332d9119dc0c
Rasmus Nilsson <info@rasmusnilsson.se>
2021-04-24 14:23:51 +0200

    merge

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import Chart from "./components/molecules/chart"
+
 function App() {
+  const data = [
+    { mood: 4, reported: "2016-06-12" },
+    { mood: 1, reported: "2016-06-16" },
+    { mood: 3, reported: "2016-06-17" },
+    { mood: 4, reported: "2016-06-19" },
+    { mood: 5, reported: "2016-06-20" },
+  ]
   return (
     <div className="App">
-      
+      <Chart data={data} />
     </div>
   );
 }
