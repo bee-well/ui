15d41af35b2cbe5c6286c9df1abee7635c530068
Rasmus Nilsson <info@rasmusnilsson.se>
2021-04-24 15:26:40 +0200

    added chart again :)))

---
+++ src/components/molecules/chart/chart.js
@@ -0,0 +1,86 @@
+import ApexCharts from "react-apexcharts"
+
+const Chart = ({ data }) => {
+    const x = data.map(mood => mood.reported)
+    const y = data.map(mood => mood.mood)
+
+    return (
+        <ApexCharts 
+            series={[
+                {
+                    name: "Mood",
+                    data: y
+                }
+            ]}
+
+            options={{
+                chart: {
+                    type: 'line',
+                    toolbar: {
+                        show: true,
+                        tools: {
+                            download: false,
+                            zoom: false,
+                            reset: false,
+                        }
+                    }
+                },
+                stroke: {
+                    width: 7,
+                    curve: 'smooth'
+                },
+                xaxis: {
+                    type: 'datetime',
+                    categories: x,
+                    tickAmount: 1,
+                    labels: {
+                        formatter: function(value, timestamp, opts) {
+                            return opts.dateFormatter(new Date(timestamp), 'dd/M')
+                        }
+                    }
+                },
+                title: {
+                    text: '',
+                    align: 'left',
+                    style: {
+                        fontSize: "16px",
+                        color: '#666'
+                    }
+                },
+                fill: {
+                    colors: ["#F59E11"],
+                    type: 'gradient',
+                    gradient: {
+                        shade: 'dark',
+                        type: 'horizontal',
+                        shadeIntensity: 0.5,
+                        gradientToColors: ['#F39C12'],
+                        inverseColors: true,
+                        opacityFrom: 1,
+                        opacityTo: 1,
+                        stops: [100, 100]
+                    }
+                },
+                markers: {
+                    size: 4,
+                    colors: ["#aaa"],
+                    strokeColors: "#fff",
+                    strokeWidth: 2,
+                    hover: {
+                        size: 7,
+                    }
+                },
+                yaxis: {
+                    min: 1,
+                    max: 5,
+                    tickAmount: 4,
+                    title: {
+                        text: '',
+                    },
+                }
+            }}
+        />
+    )
+}
+
+export default Chart
