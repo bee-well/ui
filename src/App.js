ec2cb730e3e9c6ef6e45691e1015aabb89a9a1e9
Rasmus Nilsson <info@rasmusnilsson.se>
2021-04-29 13:19:17 +0200

    cleaned app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import ProfileCard from '../src/components/organisms/cards/profile-card/profile-card'
 function App() {
   return (
     <div className="App">
-      <ProfileCard email="info@rasmusnilsson.se" name="Erik Rasmus Nilsson" />
     </div>
   );
 }
