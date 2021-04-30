88a0ac5aa7e3306f9389af478ac6e8aec527a668
Håkan Reinholdson <hreinholdson@hotmail.com>
2021-04-30 11:00:09 +0200

    Added tags selection
    Co-authored-by: Lisacsatho <Lisacsatho@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import ReportMoodModal from "./components/organisms/modals/report-mood-modal";
-
 function App() {
   return (
     <div className="App">
-      <ReportMoodModal open tagOptions={["happy", "horney", "in need of cock"]}/>
     </div>
   );
 }
